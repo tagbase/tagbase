57e202e8bdfebee3553c61820b74172e8112b44c
Tim Lam <tagtuna@gmail.com>
2025-03-06 10:19:30 +0800

    Modified GPE3 function from rgeos to sf

--- tags2etuff/getCtr_gpe3.r
+++ tags2etuff/getCtr_gpe3.r
@@ -1,15 +1,47 @@
-getCtr_gpe3 <- function(ncFile, csvFile, threshold = 50, makePlot=T, scaling = 1)
+### Jan 2025
+###----------
+# https://github.com/camrinbraun/tags2etuff/commit/37966ac6445ad66542c9ad282d3caf6d8b1a27de
+# add sf compatibility for GPE3 contours
+###----------
+
+#' Get specified contour coordinates
+#'
+#' \code{getCtr} uses GPE3 12 hour likelihood output to calculate coordinates at a
+#' specified contour of each posterior distribution of the state
+#'
+#' @param ncFile is output nc from GPE3
+#' @param threshold numeric indicating the percent contour of interest. Default
+#'   is 50 percent.
+#' @param makePlot is logical indicating whether or not to plot at each iteration
+#'
+#' @return a list of length T, dim(distr)[2], containing 1) coordinates of the
+#'   contour at each time, t. 2) the x and y distance to that contour from the
+#'   mean of the distribution (lat/lon in track). 3) reference coordinate from
+#'   mean of distribution. 4) the points of intersection of the contour by which
+#'   distance is measured in x and y
+#' @export
+#' @importFrom grDevices colorRampPalette
+#' @importFrom grDevices contourLines
+#' @importFrom graphics axis box image layout lines mtext par plot.new points
+
+getCtr_gpe3 <- function(ncFile, csvFile, threshold = 50, makePlot=FALSE, scaling = 1)
 {
   ### --- Begin Tim's edit history  --- ###
   ### 2022-03-10
   ### a) add "scaling" to scale the resolution of Raster.big 
   ### b) handle empty lat/lon in csvFile
+  ### 2025-03-06
+  ### c) rgeos has been retired, switched to sf
   ### --- End of Tim's edit history --- ###
+  
+  require(sf)
+
   nc <- RNetCDF::open.nc(ncFile)
   mtime <- as.POSIXct(RNetCDF::var.get.nc(nc, 'twelve_hour_timestamps'), origin='1970-01-01', tz='UTC')
   s <- raster::stack(ncFile, varname="twelve_hour_likelihoods")
 
   #To interpolate a surface (resample it at a higher resolution):
+  ### Tim added 'scaling' here
   Raster.big <- raster::raster(ncol=400*scaling, nrow=300*scaling, ext=raster::extent(s)) #creates the higher resolution grid
 
   Raster.HR <- raster::resample(x=s, y=Raster.big, method="bilinear")
@@ -55,7 +87,6 @@ getCtr_gpe3 <- function(ncFile, csvFile, threshold = 50, makePlot=T, scaling = 1
     Raster.breaks <- c(RasterVals[max(which(cumsum(RasterVals) <= threshold))])
 
     #print(i)
-	#print(Raster.breaks)
     ctr <- raster::rasterToContour(Raster.HR[[i]], levels=Raster.breaks)
     #ctr <- contourLines(lon, lat, s[[i]])
     #idx <- which.min(lapply(ctr, FUN=function(x) which(round(x$level,1) == round(threshold, 1))) == 1)
@@ -63,27 +94,35 @@ getCtr_gpe3 <- function(ncFile, csvFile, threshold = 50, makePlot=T, scaling = 1
     #sp::coordinates(ctr.i) <- ~x+y
     #l1 <- sp::SpatialLines(list(sp::Lines(sp::Line(sp::coordinates(ctr.i)), "L1")))
     l1 <- sp::as.SpatialLines.SLDF(ctr)
-    
+
 	### --- Tim's edits 2022-03-10: tr$lon or tr$lat somehow is NA in WC's output nc file --- ###
 	inY = NULL
 	inX = NULL
 	if (!is.na(tr$lon[i]) | !is.na(tr$lat[i])){
 	### --- End of Tim's edits --- ###	
-      hl <- data.frame(matrix(c(tr$lon[i] - 10, tr$lon[i], tr$lon[i] + 10, rep(tr$lat[i], 3)), ncol=2))
-      names(hl) <- c('x','y')
-      sp::coordinates(hl) <- ~x+y
-      hl <- sp::SpatialLines(list(sp::Lines(sp::Line(sp::coordinates(hl)), 'hl')))
 
-      vl <- data.frame(matrix(c(rep(tr$lon[i], 3), tr$lat[i] - 10, tr$lat[i], tr$lat[i] + 10), ncol=2))
-      names(vl) <- c('x','y')
-      sp::coordinates(vl) <- ~x+y
-      vl <- sp::SpatialLines(list(sp::Lines(sp::Line(sp::coordinates(vl)), 'vl')))
+    hl <- data.frame(matrix(c(tr$lon[i] - 10, tr$lon[i], tr$lon[i] + 10, rep(tr$lat[i], 3)), ncol=2))
+    names(hl) <- c('x','y')
+    sp::coordinates(hl) <- ~x+y
+    hl <- sp::SpatialLines(list(sp::Lines(sp::Line(sp::coordinates(hl)), 'hl')))
 
-      inY <- rgeos::gIntersection(l1, vl)
-      inX <- rgeos::gIntersection(l1, hl)
-	}
-	
-    if (is.null(inX) | is.null(inY)){
+    vl <- data.frame(matrix(c(rep(tr$lon[i], 3), tr$lat[i] - 10, tr$lat[i], tr$lat[i] + 10), ncol=2))
+    names(vl) <- c('x','y')
+    sp::coordinates(vl) <- ~x+y
+    vl <- sp::SpatialLines(list(sp::Lines(sp::Line(sp::coordinates(vl)), 'vl')))
+
+    l1s <- as(l1, "sf")
+    vls <- as(vl, 'sf')
+    inYs <- sf::st_intersection(l1s, vls)
+
+    hls <- as(hl, 'sf')
+    inXs <- sf::st_intersection(l1s, hls)
+
+    #inY <- rgeos::gIntersection(l1, vl)
+    #inX <- rgeos::gIntersection(l1, hl)
+    } ### --- End of Tim's edits --- ###	
+    
+    if (nrow(inXs) == 0 | nrow(inYs) == 0){
       out[[i]] <- list(ctr=l1, yDist=NA, xDist=NA, loc=tr[i,], inY=NA, inX=NA)
 
     } else{
@@ -97,32 +136,32 @@ getCtr_gpe3 <- function(ncFile, csvFile, threshold = 50, makePlot=T, scaling = 1
         lines(hl)
         lines(vl)
         points(tr$lon[i], tr$lat[i])
-        points(inY)
-        points(inX)
+        points(inYs)
+        points(inXs)
 
         # get user input to proceed
         invisible(readline(prompt="Press [enter] to perform the next iteration and plot"))
       }
 
-      if(dim(inY@coords)[1] == 1){
+      if(dim(st_coordinates(inYs))[1] == 1){
         tr.i <- data.frame(matrix(c(tr$lon[i], tr$lat[i]),ncol=2))
         names(tr.i) <- c('x','y')
         sp::coordinates(tr.i) <- ~x+y
-        yDist <- sp::spDists(tr.i, inY)
+        yDist <- sp::spDists(tr.i, as(inYs, 'Spatial'))
       } else{
-        yDist <- sp::spDists(inY)[1,2] / 2 # Euclidean, in degrees
+        yDist <- sp::spDists(as(inYs, 'Spatial'))[1,2] / 2 # Euclidean, in degrees
       }
 
-      if(dim(inX@coords)[1] == 1){
+      if(dim(st_coordinates(inXs))[1] == 1){
         tr.i <- data.frame(matrix(c(tr$lon[i], tr$lat[i]),ncol=2))
         names(tr.i) <- c('x','y')
         sp::coordinates(tr.i) <- ~x+y
-        xDist <- sp::spDists(tr.i, inX)
+        xDist <- sp::spDists(tr.i, as(inXs, 'Spatial'))
       } else{
-        xDist <- sp::spDists(inX)[1,2] / 2 # Euclidean, in degrees
+        xDist <- sp::spDists(as(inXs, 'Spatial'))[1,2] / 2 # Euclidean, in degrees
       }
 
-      out[[i+1]] <- list(ctr=l1, yDist=yDist, xDist=xDist, loc=tr[i,], inY=inY, inX=inX)
+      out[[i+1]] <- list(ctr=l1s, yDist=yDist, xDist=xDist, loc=tr[i,], inY=inYs, inX=inXs)
 
     }
 
@@ -134,6 +173,3 @@ getCtr_gpe3 <- function(ncFile, csvFile, threshold = 50, makePlot=T, scaling = 1
   return(out)
 
 }
-
-
-
